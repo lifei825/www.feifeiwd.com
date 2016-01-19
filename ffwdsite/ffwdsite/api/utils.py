d320db51d44326f4cb48e92a93e1a47f81f87f22
root <root@WEB.(none)>
2016-01-19 18:11:36 +0800

    add 51news

--- ffwdsite/ffwdsite/api/utils.py
+++ ffwdsite/ffwdsite/api/utils.py
@@ -3,10 +3,20 @@ from django.shortcuts import render
 from django.views.generic.base import View  
 import random
 import os
+import redis
+import json
 
 
 class ChangePicture(View):
     def get(self, request):
         index_img = 'hz/'+random.choice(os.listdir('/usr/html/static/img/index/hz'))
         return HttpResponse(index_img)
+
+class Get51News(View):
+    def get(self, request):
+        r9 = redis.Redis(host='127.0.0.1', port=6379, db=9, password='ffwd')
+        key = request.GET.get('key')
+        data = r9.lrange(key, 0, -1)
+        lists = [eval(d) for d in data]
+        return HttpResponse(json.dumps(lists), content_type="application/json")
 
--- ffwdsite/ffwdsite/urls.py
+++ ffwdsite/ffwdsite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView 
 from django.contrib.auth.views import login,logout 
 from contact.views import RegisterView,LoginView,Comment
-from api.utils import ChangePicture
+from api.utils import ChangePicture, Get51News
 
 
 urlpatterns = patterns('',
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     (r'^logout/$', logout,{'next_page': '/'}),
     (r'^comment/$', Comment),
     (r'^api/changePicture$', ChangePicture.as_view()),
+    (r'^api/get51news$', Get51News.as_view()),
     # test
     url(r'^portrait_img/(?P<path>.*.jpg)/$', "django.views.static.serve", {"document_root":"/home/django/ffwdsite/img/portrait_img/"}),
     (r'^wu/$', TemplateView.as_view(template_name="index.html"))
